61b7b8bd444e34fc98aedd03f4fdf8c5de382950
Joaquin Serrano <jserrano.vd@gmail.com>
2020-11-23 07:44:44 -0300

    upload cheatsheet

--- cheatSheets/files.go
+++ cheatSheets/files.go
@@ -305,4 +305,37 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	/////////////////////////////////
+	// Reading From Standard Input (console)
+	// Go Playground: https://play.golang.org/p/n8JuneN40_p
+	/////////////////////////////////
+
+	// creating a scanner
+	scanner := bufio.NewScanner(os.Stdin) //os.Stdin reads the output from the command line
+	fmt.Printf("%T\n", scanner)           //pointer to bufio.scanner
+
+	scanner.Scan() //it waits for the input and buffers the input untill a new line
+
+	// gettting the scanned data
+	text := scanner.Text()   // string type
+	bytes := scanner.Bytes() // uint8[] slice type
+
+	fmt.Println("Input text:", text)
+	fmt.Println("Input bytes:", bytes)
+
+	// reading the input continously until a specific string is scanned
+	for scanner.Scan() {
+		text = scanner.Text()
+		fmt.Println("You entered:", text)
+		if text == "exit" {
+			fmt.Println("Exiting the scanning ...")
+			break
+		}
+	}
+
+	// error handling
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
 }
