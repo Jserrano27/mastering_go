812b9c7b892f36c04a4a0f28c59f97d4be251081
Joaquin Serrano <jserrano.vd@gmail.com>
2020-12-05 16:49:55 -0300

    upload cheatsheet

--- cheatSheets/interfaces.go
+++ cheatSheets/interfaces.go
@@ -54,6 +54,15 @@ func print(s shape) {
 	fmt.Printf("Perimeter: %v\n", s.perimeter())
 }
 
+// declaring an empty interface
+type emptyInterface interface {
+}
+
+// declaring a new struct type which has one field of type empty interface
+type person struct {
+	info interface{}
+}
+
 func main() {
 
 	// declaring a circle and a rectangle values
@@ -99,4 +108,35 @@ func main() {
 		fmt.Printf("%#v has rectangle type\n", value)
 
 	}
+
+	// declaring an empty interface value
+	var empty interface{}
+
+	// an empty interface may hold values of any type
+	// storing an int in the empty interface
+	empty = 5
+	fmt.Println(empty) // => 5
+
+	// storing a string in the empty interface
+	empty = "Go"
+	fmt.Println(empty) // => Go
+
+	// storing a slice in the empty interface
+	empty = []int{2, 34, 4}
+	fmt.Println(empty) // => [2 34 4]
+
+	// fmt.Println(len(empty)) -> error, and it doesn't work!
+
+	// retrieving the dynamic value using an assertion
+	fmt.Println(len(empty.([]int))) // => 3
+
+	// declaring person value
+	you := person{}
+
+	// assigning any value to empty interface field
+	you.info = "You name"
+	you.info = 40
+	you.info = []float64{4.5, 6., 8.1}
+
+	fmt.Println(you.info)
 }
